7ef45cacc2aa598f99cf5a978df96517e00470e2
Kai E. Frøland <kfrol@pm.me>
2022-05-07 11:57:15 +0200

    kkit-cli: First commit

---
+++ kkit-cli/source/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include "../../klabkit-sdl/source/klabkit/compression.h"
+
+int main(int argc, char* argv[]) {
+
+	for (int i{ 0 }; i < argc; ++i) {
+
+	}
+}
